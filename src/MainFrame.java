265e01ea5ff97fa4086682dfe9aa5afe779f19df
Devinthedude515 <166536125+Devinthedude515@users.noreply.github.com>
2026-09-02 14:32:08 -0400

    Setting up the base of the project

---
+++ src/MainFrame.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+
+    //create panel object
+    JPanel mainPanel = new JPanel();
+
+    public MainFrame() {
+
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.setBackground(new Color(128, 128, 255));
+    }
+    
+    //initialize the frame
+    public void initialaize() {
+        setTitle("My Application");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+}
