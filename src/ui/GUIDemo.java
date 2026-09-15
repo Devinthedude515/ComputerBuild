c29945424150cec3f8405ac72c2a7514d7966f52
devin <166536125+Devinthedude515@users.noreply.github.com>
2026-09-15 12:10:31 -0400

    Gaining understanding on flow layout and Content pane

--- src/ui/GUIDemo.java
+++ src/ui/GUIDemo.java
@@ -1,31 +1,47 @@
 package ui;
 
 //imports
+import java.awt.Container;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class GUIDemo {
 
+    //fields
     private JFrame frame;
     private int width;
     private int height;
-    private JButton button;
+    private JButton button1, button2;
+    private JTextField input;
+    private JLabel label;
 
 
     //create constructor
     public GUIDemo(int w, int h) {
         frame = new JFrame();
-        button = new JButton("Click Me");
+        label = new JLabel("Hello");
+        input = new JTextField(10);
+        button1 = new JButton("Button 1");
+        button2 = new JButton("Button 2");
         width = w;
         height = h;
     }
 
     public void setUpGUI() {
+
         //set the frame up
+        Container cp = frame.getContentPane();
+        FlowLayout flow = new FlowLayout();
+        cp.setLayout(flow);
+        
         frame.setSize(width, height);
         frame.setTitle("GUI Demo");
-        frame.add(button);
+        cp.add(input);
+        cp.add(label);
+        cp.add(button1);
+        cp.add(button2);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
@@ -41,6 +57,6 @@ public class GUIDemo {
         };
 
         //attach our button to the listener
-        button.addActionListener(buttonListener);
+        button1.addActionListener(buttonListener);
     }
 }
